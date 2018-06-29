866a859b474a2a307530343b2100efc891591b9e
Anthony Jensen <aj.coloradoav@gmail.com>
2018-06-29 12:58:27 -0600

    Add project files.

---
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+	public class ComicBooksController:Controller
+	{
+		public ContentResult Detail()
+		{
+			if(Time
+			return new ContentResult()
+			{
+				Content = "hello from the comic books controller!"
+			};
+		}
+	}
+}
